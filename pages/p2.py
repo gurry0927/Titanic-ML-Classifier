417551bb197139e4a76ee84ba05b71cc3a9ced66
王惟成 <32692759+gurry0927@users.noreply.github.com>
2025-08-24 23:33:15 +0800

    Add files via upload

--- pages/p2.py
+++ pages/p2.py
@@ -213,7 +213,7 @@ with tab1:
     st.subheader("🛠️ 資料清理與預處理流程")
     # 流程圖片
     try:
-        col1, col2, col3 = st.columns([1, 2, 1])
+        col1, col2, col3 = st.columns([1.25, 2.5, 1.25])
         with col2:
             st.image('images/data_cleaned.png', caption="資料清理流程圖", use_container_width=True)
     except:
